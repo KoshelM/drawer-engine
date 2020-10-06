cf6b7ebca50bc221ec99ec1d80e1d3768e26e373
KoshelM <69363646+KoshelM@users.noreply.github.com>
2020-10-06 16:21:56 +0300

    bug fix

--- src/components/table/controls/radiobutton-control.vue
+++ src/components/table/controls/radiobutton-control.vue
@@ -1,8 +1,8 @@
 <template>
   <div>
     <div v-for="option in value.options" :key="option.value">
-      <input type="radio" v-bind:value="option.value" v-model="value.selected" />
-      <label >{{option.label}}</label>
+      <input type="radio" v-bind:id="option.value + id" v-bind:value="option.value" v-model="value.selected" />
+      <label v-bind:for="option.value + id">{{option.label}}</label>
     </div>
   </div>
 </template>>
@@ -10,6 +10,9 @@
 <script>
 import ControlMixin from "./control.mixin";
 export default {
-  mixins: [ControlMixin]
+  mixins: [ControlMixin],
+  created(){
+      this.id = Math.random();
+  }
 };
 </script>
